c07cb649455eec9047115d765e7ca165114dcff4
Trsh16 <94509391+Trsh16@users.noreply.github.com>
2025-07-26 11:35:49 +0700

    Initial Commit

---
+++ MusicForPeople/MusicForPeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicForPeopleApp.swift
+//  MusicForPeople
+//
+//  Created by Trisha Alexis Likorawung on 26/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicForPeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicForPeopleTests/MusicForPeopleTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicForPeopleTests.swift
+//  MusicForPeopleTests
+//
+//  Created by Trisha Alexis Likorawung on 26/07/25.
+//
+
+import Testing
+@testable import MusicForPeople
+
+struct MusicForPeopleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
